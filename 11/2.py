759b0204d3dadc693af1753eacfbb7bcc5c00449
Hari <hprasad2709@gmail.com>
2023-12-11 13:41:14 +0000

    made part 2 5 times more efficient after going through the megathread

--- 11/2.py
+++ 11/2.py
@@ -3,29 +3,40 @@ from itertools import combinations
 
 
 def main():
-    EXPANSION_FACTOR = 1000000
+    EXPANSION_FACTOR = 1_000_000
 
     image = Grid([row.strip("\n") for row in open("input.txt", "r").readlines()])
-    galaxies = image.find("#")
 
+    galaxies = image.find("#")
     galaxy_cols, galaxy_rows = [set(t) for t in list(zip(*galaxies))]
 
-    empty_rows = set(range(image.NUM_ROWS)).difference(galaxy_rows)
-    empty_cols = set(range(image.NUM_COLS)).difference(galaxy_cols)
+    expansion_counts_cols = []
+    count = 0
+    for i in range(image.NUM_COLS):
+        expansion_counts_cols.append(count)
+
+        if i not in galaxy_cols:
+            count += 1
+
+    expansion_counts_rows = []
+    count = 0
+    for i in range(image.NUM_ROWS):
+        expansion_counts_rows.append(count)
+
+        if i not in galaxy_rows:
+            count += 1
 
     distance_sum = 0
     for g1, g2 in combinations(galaxies, 2):
-        manhattan_distance = abs(g2[1] - g1[1])
-        for row in empty_rows:
-            if g1[1] < row < g2[1] or g2[1] < row < g1[1]:
-                manhattan_distance += EXPANSION_FACTOR - 1
+        row_dist = abs(g2[1] - g1[1]) + (EXPANSION_FACTOR - 1) * abs(
+            expansion_counts_rows[g2[1]] - expansion_counts_rows[g1[1]]
+        )
 
-        manhattan_distance += abs(g2[0] - g1[0])
-        for col in empty_cols:
-            if g1[0] < col < g2[0] or g2[0] < col < g1[0]:
-                manhattan_distance += EXPANSION_FACTOR - 1
+        col_dist = abs(g2[0] - g1[0]) + (EXPANSION_FACTOR - 1) * abs(
+            expansion_counts_cols[g2[0]] - expansion_counts_cols[g1[0]]
+        )
 
-        distance_sum += manhattan_distance
+        distance_sum += row_dist + col_dist
 
     print(distance_sum)
 
